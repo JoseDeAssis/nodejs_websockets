94e853ec9e70e39de130a00e3fcb18be1d690c12
JoseDeAssis <jose.maria.assis23@gmail.com>
2024-05-30 21:09:55 -0300

    início do curso de node com websockets

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import url from "url";
+import path from "path";
+import http from "http";
+import { Server } from 'socket.io';
+
+const app = express();
+const PORT = process.env.port || 3000;
+const currentPath = url.fileURLToPath(import.meta.url);
+const publicDir = path.join(currentPath, "../..", "public");
+
+app.use(express.static(publicDir));
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, () => {
+  console.log("Servidor iniciado!");
+})
+
+const io = new Server(httpServer);
+
+io.on("connection", () => {
+  console.log("Um cliente se conectou");
+})
